dbd26b4236fa6f15277c7fe2b9275a7ffcb82a6c
Nikita Maltsev <nisa4414@gmail.com>
2023-06-08 02:27:34 -0700

    Added merge-arrays.js

---
+++ merge-arrays.js
@@ -0,0 +1,10 @@
+'use strict'
+// @ts-check
+
+function merge(arr1, arr2) {
+  return [...arr1, ...arr2].sort((a, b) => a - b)
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
